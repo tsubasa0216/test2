1f490909e246fa31e4654b1bbeeaa1d7701af17b
tsubasa0216 <70942087+tsubasa0216@users.noreply.github.com>
2020-10-05 08:38:42 +0900

    my answer

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,9 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    if amount % 2 == 0
+      3 * (amount - 2) / 2 + 2
+    elsif amount % 2 == 1
+      3 * (amount - 1) / 2 + 1
+    end
   end
 end
